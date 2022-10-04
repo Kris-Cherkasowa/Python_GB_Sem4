9571c996082c6eb03e7411abde31eb612a03ebcd
Cherkasova Kristina <kris.cherkasowa2017@yandex.ru>
2022-10-04 17:21:53 +0300

    Task_4

---
+++ Task_4.py
@@ -0,0 +1,27 @@
+#Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+import itertools
+from random import randint
+
+
+def get_ratios(k):
+    ratios = [randint(0, 10) for i in range (k + 1)]
+    while ratios[0] == 0:
+        ratios[0] = randint(1, 10) 
+    return ratios
+
+def get_polynomial(k, ratios):
+    var = ['*x^']*(k-1) + ['*x']
+    polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue = '') if a !=0]
+    for x in polynomial:
+        x.append(' + ')
+    polynomial = list(itertools.chain(*polynomial))
+    polynomial[-1] = ' = 0'
+    return "".join(map(str, polynomial)).replace(' 1*x',' x')
+
+k = int(input('Введите натуральную степень k:\n'))
+ratios = get_ratios(k)
+polynom = get_polynomial(k, ratios)
+
+with open('Task_4.txt', 'w') as data:
+    data.write(polynom)
